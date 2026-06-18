9d2fc993f3dc4a26d887ab59a1a46ad392fff585
tutugamerhsh-blip <tutugamerhsh@gmail.com>
2026-06-18 09:59:01 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -26,7 +26,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Nexus Galaxy",
         // Activity type number (0 = Playing).
-        type: 1,
+        type: 3,
       },
     ],
   },
